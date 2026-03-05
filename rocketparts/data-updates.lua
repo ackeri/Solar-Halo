7a9761eb9912e8ad07a83dd01dc0d23f3351efc7
Loki White <loki.w.white@gmail.com>
2026-03-04 17:18:59 -0800

    quick dirty fix to make rocket silo fluidboxes more compatible

--- rocketparts/data-updates.lua
+++ rocketparts/data-updates.lua
@@ -61,31 +61,12 @@ data:extend{rocketrecipe}
 local proto = data.raw["rocket-silo"]["rocket-silo"]
 for name, proto in pairs(data.raw["rocket-silo"]) do
 	proto.fixed_recipe = nil
-	if proto.fluid_boxes == nil or #proto.fluid_boxes < 2 then
-		local mi = proto.collision_box.left_top or proto.collision_box[1]
-		local ma = proto.collision_box.right_bottom or proto.collision_box[2]
-		local minx = mi.x or mi[1]
-		local maxx = ma.x or ma[1]
-		local miny = mi.y or mi[2]
-		local maxy = ma.y or ma[2]
+	if name == "rocket-silo" and proto.fluid_boxes == nil then
 		proto.fluid_boxes = {{
 				volume = 500,
 				pipe_connections = {{
 						direction = defines.direction.west,
-						position = {x = minx, y = 0}
-					},{
-						direction = defines.direction.east,
-						position = {x = maxx, y = 0}
-				}},
-				production_type = "input"
-			},{
-				volume = 500,
-				pipe_connections = {{
-						direction = defines.direction.north,
-						position = {x = 0, y = miny}
-					},{
-						direction = defines.direction.south,
-						position = {x = 0, y = maxy}
+						position = {x = minx, y = miny}
 				}},
 				production_type = "input"
 		}}
